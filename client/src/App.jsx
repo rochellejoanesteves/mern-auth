8473612ff6186d144d167d03fbf7e9536222cec0
Rochelle <rochellejoan.p.esteves@gmail.com>
2024-01-17 16:52:49 +0800

    add routes and create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/about";
+import Home from "./pages/home";
+import Profile from "./pages/profile";
+import SignIn from "./pages/signin";
+import SignOut from "./pages/signup";
 
 function App() {
   return (
     <div>
-      App
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-out" element={<SignOut />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/signup/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignOut() {
+  return (
+    <div>
+      Sign Out
+    </div>
+  )
+}
+
+export default SignOut
